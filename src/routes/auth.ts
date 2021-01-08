6be428b5a6d5b012127886efe0307daf561acc41
imri <imri.paran@gmail.com>
2021-01-08 12:03:16 +0200

    use key cloak id

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,6 +5,10 @@ const router = express.Router();
 
 router.get("/user", getUser);
 router.get("/token/:code", getToken);
-router.get("/login", (reg, res) => res.redirect(process.env.LOGIN_REDIRECT));
+router.get("/login", (reg, res) =>
+  res.redirect(
+    `${process.env.LOGIN_REDIRECT}&client_id=${process.env.KEYCLOAK_CLIENT_ID}`
+  )
+);
 
 export default router;

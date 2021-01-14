c0e2135b827779e9c03c9d56ba93c3168a1b77d2
imri <imri.paran@gmail.com>
2021-01-14 07:08:05 +0200

    added auth routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
-require('express-async-errors');
-import morgan from 'morgan';
-import routes from "./routes"
+require("express-async-errors");
+import morgan from "morgan";
+import routes from "./routes";
 import { installDevKeycloak, installKeycloak } from "./keycloak";
 import bodyParser from "body-parser";
 import { getToken } from "./cotrollers/auth";
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
 app.use((req: any, res: any, next) => {
   const { code, session_state } = req.query;
@@ -21,11 +21,12 @@ app.get("/", function (req: Request, res: Response) {
   res.send("Hello World");
 });
 
-app.get("/login", (req, res) =>
-  res.redirect(
-    `${process.env.LOGIN_REDIRECT}&client_id=${process.env.KEYCLOAK_CLIENT_ID}`
-  )
-);
+app.get("/login", (req, res) => {
+  const redirect = `${process.env.LOGIN_REDIRECT}&client_id=${process.env.KEYCLOAK_CLIENT_ID}`;
+  console.log(`redirecting to ${redirect}`);
+  res.redirect(redirect);
+});
+
 if (process.env.NODE_ENV === "production") {
   installKeycloak(app);
 } else {
@@ -33,5 +34,5 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(bodyParser.json());
-app.use("/", routes)
+app.use("/", routes);
 app.listen(port, () => console.log(`server listening on port ${port}`));

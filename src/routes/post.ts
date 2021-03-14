41bf2020fd01a30f66ef7c346d26b7a1191b249b
Imri Paran <imri.paran@gmail.com>
2021-03-14 18:07:20 +0200

    temp

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -4,6 +4,6 @@ import { postReport, randomPost } from "../cotrollers/post";
 const router = express.Router();
 
 router.post("/random", randomPost);
-router.post("/:post/report", postReport);  
+router.post("/:post/report", postReport);
 
 export default router;
